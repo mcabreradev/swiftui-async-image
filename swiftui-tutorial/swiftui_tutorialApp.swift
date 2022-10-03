0a79280abd50d6b731b8769cc8758fe08a62b69e
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2022-10-03 19:41:12 -0300

    add swiftui code

---
+++ swiftui-tutorial/swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialApp.swift
+//  swiftui-tutorial
+//
+//  Created by Migue on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
